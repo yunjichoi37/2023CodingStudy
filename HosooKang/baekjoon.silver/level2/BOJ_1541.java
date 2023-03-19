e341bbc3003f67d3dbe6ad364bb421ab05d5a176
강호수 <hosoo3513@gmail.com>
2023-03-19 23:36:43 +0900

    [BOJ] 일어버린 괄호 / 실버2 / 40분

---
+++ HosooKang/baekjoon.silver/level2/BOJ_1541.java
@@ -0,0 +1,32 @@
+package kanghosoo.baekjoon.silver.level2;
+
+import java.util.Scanner;
+
+public class BOJ_1541 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String next = scan.next();
+
+        int sum = Integer.MAX_VALUE;
+        String[] sub = next.split("-");
+
+        for (int i = 0; i < sub.length; i++) {
+            int tmp = 0;
+
+            String[] add = sub[i].split("\\+");
+
+            for (int j = 0; j < add.length; j++) {
+                tmp += Integer.parseInt(add[j]);
+            }
+
+            // 첫번째는 양수일 수도 있으니
+            if (sum == Integer.MAX_VALUE) {
+                sum = tmp;
+            } else {
+                sum -= tmp;
+            }
+        }
+        System.out.println(sum);
+    }
+}
