bb7fb87fd66476e0337528a822a76233c9865c22
sunghyun1356 <sunghyun1356@naver.com>
2023-03-20 14:16:07 +0900

    [BOJ]/실버1/20분
    https://www.acmicpc.net/problem/1080

--- 1080_행렬.py
+++ 1080_행렬.py
@@ -1,15 +1,30 @@
-import sys
-cal = input().split('-')
-result = []
-for i in cal:
-    temp=0
-    plus = i.split('+')
-    for j in plus:
-        temp +=int(j)
-    result.append(temp)
-first = result[0]
-for k in range(1,len(result)):
-    first -=result[k]
-print(first)
-    
-        
+from sys import stdin
+
+def change(mat,k,l):
+    for i in range(k,k+3):
+        for j in range(l,l+3):
+            mat[i][j] = 1 - mat[i][j]
+    return mat
+            
+
+            
+
+            
+
+
+n, m = map(int, input().split())
+matrix_first =[list(map(int, stdin.readline().rstrip()))for j in range(n)]
+matrix_second = [list(map(int, stdin.readline().rstrip()))for j in range(n)]
+
+
+count =0
+for i in range(n-2):
+    for j in range(m-2):
+        if matrix_first[i][j] != matrix_second[i][j]:
+            count+=1
+            change(matrix_first,i,j)
+if matrix_first == matrix_second:
+    print(count)
+else:
+    print(-1)
+            
